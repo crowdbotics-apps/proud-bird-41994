8ad1bdea6ba660629468c421f13dc59c89f26685
Crowdbotics <team@crowdbotics.com>
2023-07-08 02:50:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-bird-41994",
+  url: "https://proud-bird-41994.botics.co",
+  api: "https://proud-bird-41994.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
